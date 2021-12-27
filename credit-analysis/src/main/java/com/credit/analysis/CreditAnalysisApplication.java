a6a7c5f4a803ecb66eccc327084c913756809f9b
Suele <suelen.ads.guimaraes@gmail.com>
2021-12-27 16:28:52 -0300

    Adiciona as dependencias necessarias para executar o projeto e o arquivo docker compose para executar o projeto.

---
+++ credit-analysis/src/main/java/com/credit/analysis/CreditAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.credit.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditAnalysisApplication.class, args);
+	}
+
+}
---
+++ credit-analysis/src/test/java/com/credit/analysis/CreditAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.credit.analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
