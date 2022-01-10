ecb24acc84235d1842c1796c948f443f8e2f8e75
Suele <suelen.ads.guimaraes@gmail.com>
2022-01-10 01:35:51 -0300

    Adiciona os endponts para listar e detalhar os emprestimos.

--- credit-analysis/src/main/java/com/credit/analysis/controller/LoanController.java
+++ credit-analysis/src/main/java/com/credit/analysis/controller/LoanController.java
@@ -1,33 +1,53 @@
 package com.credit.analysis.controller;
 
-import com.credit.analysis.dto.LoanDTO;
+import com.credit.analysis.dto.ListLoanDTO;
+import com.credit.analysis.dto.LoanDetailDTO;
+import com.credit.analysis.model.LoanModel;
+import com.credit.analysis.repository.LoanRepository;
 import com.credit.analysis.service.LoanService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.server.handler.ResponseStatusExceptionHandler;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("loan")
 public class LoanController {
 	@Autowired
 	private LoanService loanService;
 
-	@GetMapping("/register")
-	public String formLoan(LoanDTO loanDTO) {
-		return "loan";
+	@Autowired
+	private LoanRepository loanRepository;
+
+
+	@GetMapping("/{cpf}")
+	public ResponseEntity<List<ListLoanDTO>> getCustomer(@PathVariable("cpf") String cpf) {
+		return ResponseEntity.status(200).body(loanService.listLoan(cpf));
 	}
 
-	@PostMapping("/register-new")
-	public String cresteNewLoan(@Valid LoanDTO loanDTO, BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {
-			return "loan";
+
+	@GetMapping("/detail/{cpf}")
+	public ResponseEntity<List<LoanDetailDTO>> detail(@PathVariable("cpf") String cpf) {
+		return ResponseEntity.status(200).body(loanService.loanDetail(cpf));
+	}
+
+	@PostMapping("/{customerId}")
+	public ResponseEntity<LoanModel> createNewLoan(@Valid @RequestBody LoanModel loanModel, @PathVariable("customerId") Long customerId) {
+		Optional<LoanModel> customerIdExist = loanRepository.findById(customerId);
+		if (customerIdExist.isPresent()) {
+
+			loanModel.setCustomer(customerIdExist.get().getCustomer());
+			loanModel.setAmountLoan(loanModel.getAmountLoan());
+			loanModel.setAmountPortion(loanModel.getAmountPortion());
+			loanModel.setDatePortion(loanModel.getDatePortion());
+			return ResponseEntity.status(201).body(loanService.createNewLoan(loanModel));
 		}
-		loanService.createNewLoan(loanDTO.toNewLoan());
-		return "loan";
+		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Verifique se os dados foram preenchidos corretamente.");
 	}
 }
