30027a2b17ca99c17f327692b3d1ace14ba239e9
Suele <suelen.ads.guimaraes@gmail.com>
2022-01-10 01:37:50 -0300

    Adiciona o endponit que cadastra um novo cliente.

--- credit-analysis/src/main/java/com/credit/analysis/controller/CustomerController.java
+++ credit-analysis/src/main/java/com/credit/analysis/controller/CustomerController.java
@@ -1,34 +1,23 @@
 package com.credit.analysis.controller;
 
-import com.credit.analysis.dto.CustomerDTO;
+import com.credit.analysis.dto.LoanDetailDTO;
+import com.credit.analysis.model.CustomerModel;
 import com.credit.analysis.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("customer")
 public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 
-	@GetMapping("/register")
-	public String formCustomer(CustomerDTO customerDTO) {
-		return "register";
-	}
-
-
-	@PostMapping("/register-new")
-	public String createNewCustomer(@Valid CustomerDTO customerDTO, BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {
-			return "register";
-		}
-		customerService.createNewCustomer(customerDTO.toNewCustoner());
-		return "register";
+	@PostMapping
+	public ResponseEntity<CustomerModel> createNewCustomer(@Valid @RequestBody CustomerModel customerModel) {
+		return ResponseEntity.status(201).body(customerService.createNewCustomer(customerModel));
 	}
 }
